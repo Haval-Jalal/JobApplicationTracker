3951e829c3e6f07e751c8c4ec0c7f9e035dfb84e
Haval-Jalal <Haval.jalal@hotmail.com>
2025-10-14 20:03:33 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿namespace JobApplicationTracker
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Börjar med att skapa 2 klasser direkt, En klass som heter JobApplication där jag ska har en enum, 6 attributer och 2 metoder.
+            //Den andra klassen ska heta JobManager som ska ha 1 attribut som sedan ska göras till en lista samt 5 metoder.
+
+            
+        }
+    }
+}
